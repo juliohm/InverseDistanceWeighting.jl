0e06433725f88419645870f2691b32e0c64a8b88
Júlio Hoffimann <julio.hoffimann@gmail.com>
2017-11-06 21:07:52 -0800

    Add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,36 @@
+using GeoStats
 using InverseDistanceWeighting
+using Plots; gr()
 using Base.Test
+using VisualRegressionTests
 
-# no tests
+# setup GR backend for Travis CI
+ENV["GKSwstype"] = "100"
+
+# list of maintainers
+maintainers = ["juliohm"]
+
+# environment settings
+ismaintainer = "USER" ∈ keys(ENV) && ENV["USER"] ∈ maintainers
+istravislinux = "TRAVIS" ∈ keys(ENV) && ENV["TRAVIS_OS_NAME"] == "linux"
+datadir = joinpath(@__DIR__,"data")
+
+@testset "Basic problem" begin
+  geodata = GeoDataFrame(DataFrames.DataFrame(x=[25.,50.,75.], y=[25.,75.,50.], variable=[1.,0.,1.]), [:x,:y])
+  domain = RegularGrid{Float64}(100,100)
+  problem = EstimationProblem(geodata, domain, :variable)
+
+  solver = InvDistWeight()
+
+  solution = solve(problem, solver)
+
+  if ismaintainer || istravislinux
+    function plot_solution(fname)
+      plot(solution, size=(1000,400))
+      png(fname)
+    end
+    refimg = joinpath(datadir,"solution.png")
+
+    @test test_images(VisualTest(plot_solution, refimg), popup=!istravislinux) |> success
+  end
+end
