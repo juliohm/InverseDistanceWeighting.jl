bcba82c774df5ae5572b63cef502911cbe1bef85
Júlio Hoffimann <julio.hoffimann@gmail.com>
2018-08-24 15:30:27 -0700

    More fixes for Julia v0.7

--- src/InverseDistanceWeighting.jl
+++ src/InverseDistanceWeighting.jl
@@ -54,47 +54,47 @@ function solve(problem::EstimationProblem, solver::InvDistWeight)
     # number of data points for variable
     ndata = length(z)
 
+    @assert ndata > 0 "estimation requires data"
+
     # allocate memory
     varμ = Vector{V}(undef, npoints(pdomain))
     varσ = Vector{V}(undef, npoints(pdomain))
 
-    if ndata > 0
-      # fit search tree
-      kdtree = KDTree(X, varparams.distance)
+    # fit search tree
+    kdtree = KDTree(X, varparams.distance)
 
-      # keep track of estimated locations
-      estimated = falses(npoints(pdomain))
+    # keep track of estimated locations
+    estimated = falses(npoints(pdomain))
 
-      # consider data locations as already estimated
-      for (loc, datloc) in datamap(problem, var)
-        estimated[loc] = true
-        varμ[loc] = value(pdata, datloc, var)
-        varσ[loc] = zero(V)
-      end
+    # consider data locations as already estimated
+    for (loc, datloc) in datamap(problem, var)
+      estimated[loc] = true
+      varμ[loc] = value(pdata, datloc, var)
+      varσ[loc] = zero(V)
+    end
 
-      # determine number of nearest neighbors to use
-      k = varparams.neighbors == nothing ? ndata : varparams.neighbors
+    # determine number of nearest neighbors to use
+    k = varparams.neighbors == nothing ? ndata : varparams.neighbors
 
-      @assert k ≤ ndata "number of neighbors must be smaller or equal to number of data points"
+    @assert k ≤ ndata "number of neighbors must be smaller or equal to number of data points"
 
-      # pre-allocate memory for coordinates
-      coords = MVector{ndims(pdomain),coordtype(pdomain)}(undef)
+    # pre-allocate memory for coordinates
+    coords = MVector{ndims(pdomain),coordtype(pdomain)}(undef)
 
-      # estimation loop
-      for location in SimplePath(pdomain)
-        if !estimated[location]
-          coordinates!(coords, pdomain, location)
+    # estimation loop
+    for location in SimplePath(pdomain)
+      if !estimated[location]
+        coordinates!(coords, pdomain, location)
 
-          idxs, dists = knn(kdtree, coords, k)
+        idxs, dists = knn(kdtree, coords, k)
 
-          weights = one(V) ./ dists
-          weights /= sum(weights)
+        weights = one(V) ./ dists
+        weights /= sum(weights)
 
-          values = view(z, idxs)
+        values = view(z, idxs)
 
-          varμ[location] = sum(weights[i]*values[i] for i in eachindex(values))
-          varσ[location] = minimum(dists)
-        end
+        varμ[location] = sum(weights[i]*values[i] for i in eachindex(values))
+        varσ[location] = minimum(dists)
       end
     end
 
