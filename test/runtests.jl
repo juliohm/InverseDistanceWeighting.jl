722eed1b9ba7af4ac92a8bdf4a5cb719f36aecf7
Júlio Hoffimann <julio.hoffimann@gmail.com>
2019-05-06 21:40:23 -0300

    Update test data

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,39 +1,33 @@
 using GeoStatsBase
 using GeoStatsDevTools
 using InverseDistanceWeighting
-using Plots; gr()
+using Plots; gr(size=(1000,400))
 using VisualRegressionTests
 using Test, Pkg
 
-# list of maintainers
-maintainers = ["juliohm"]
+# workaround GR warnings
+ENV["GKSwstype"] = "100"
 
 # environment settings
+islinux = Sys.islinux()
 istravis = "TRAVIS" ∈ keys(ENV)
-ismaintainer = "USER" ∈ keys(ENV) && ENV["USER"] ∈ maintainers
 datadir = joinpath(@__DIR__,"data")
-
-if ismaintainer
+visualtests = !istravis || (istravis && islinux)
+if !istravis
   Pkg.add("Gtk")
   using Gtk
 end
 
 @testset "Basic problem" begin
   geodata = PointSetData(Dict(:variable => [1.,0.,1.]), [25. 50. 75.;  25. 75. 50.])
-  domain = RegularGrid{Float64}(100,100)
+  domain  = RegularGrid{Float64}(100,100)
   problem = EstimationProblem(geodata, domain, :variable)
 
   solver = InvDistWeight()
 
   solution = solve(problem, solver)
 
-  if ismaintainer || istravis
-    function plot_solution(fname)
-      plot(solution, size=(1000,400))
-      png(fname)
-    end
-    refimg = joinpath(datadir,"solution.png")
-
-    @test test_images(VisualTest(plot_solution, refimg), popup=!istravis, tol=0.1) |> success
+  if visualtests
+    @plottest plot(solution) joinpath(datadir,"solution.png") !istravis
   end
 end
